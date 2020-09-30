6941901d03e868860d3b886fdb25c6e1ab6d6e00
HannaHerman <mrs.hanna.herman@gmail.com>
2020-09-30 16:04:18 +0300

    init: solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minVal = 0;
+  if (array === undefined || array.length === 0) {
+    return minVal;
+  }
+  else {
+    array = array.sort(sortNumbers);
+    minVal = array[0];
+    return minVal;
+  }
+  
+  function sortNumbers(a, b) {
+    if (a > b) {
+      return 1;
+    } 
+    else if (b > a) {
+      return -1;
+    } 
+    else {
+      return 0;
+    }
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxVal = 0;
+  if (array === undefined || array.length === 0) {
+    return maxVal;
+  }
+  else {
+    array = array.sort(sortNumbers);
+    maxVal = array[array.length - 1];
+    return maxVal;
+  }
+  
+  function sortNumbers(a, b) {
+    if (a > b) {
+      return 1;
+    } 
+    else if (b > a) {
+      return -1;
+    } 
+    else {
+      return 0;
+    }
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  else {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  }
 }
